6a65de775109f28b87b70687ebcf5f3b64a6d958
Nicolas <nicolas@racchi.com>
2021-01-21 21:14:00 +0100

    Added og image

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import Routes from "@src/Routes";
 import { Provider } from "@context/global";
 import reducer, { initialState } from "@context/reducer";
 
-function App() {
+const App = () => {
   return (
     <Provider initialState={initialState} reducer={reducer}>
       <Helmet>
         <title>SaaS4Devs</title>
+        <meta
+          name="description"
+          content="An awesome collection of content for developers trying to bootstrap
+            their SaaS business. Free & Open source from day one."
+        />
         <meta property="og:url" content="https://saas4devs.tech" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="SaaS4Devs" />
@@ -19,14 +24,17 @@ function App() {
           content="An awesome collection of content for developers trying to bootstrap
             their SaaS business. Free & Open source from day one."
         />
-        <meta property="og:image" content={require("@assets/saas4devs.png")} />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/48695862/105406874-4ec8d380-5c2d-11eb-8fdd-0d789949d5d8.png"
+        />
       </Helmet>
-      {/* Add React.Suspense support */}
+
       <Router>
         <Routes />
       </Router>
     </Provider>
   );
-}
+};
 
 export default App;
